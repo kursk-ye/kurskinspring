1162b02281cf722c9d4871d956eaeea155be8fa1
yelei <kursk.ye@gmail.com>
2022-04-18 23:26:08 +0800

    modify ch02

--- ch02/src/main/java/tacos/TacoCloudApplication.java
+++ ch02/src/main/java/tacos/TacoCloudApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication   // <1>
 public class TacoCloudApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TacoCloudApplication.class, args); // <2>
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(TacoCloudApplication.class, args); // <2>
+  }
 
 }
